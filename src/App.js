dcb7398d3336dc5a98d941ba0ab33f24398bc288
Aleksandra <aleksandra.tobiszewska@softwarehut.com>
2019-04-09 11:35:11 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from './components/Main/Main';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={props => <Main {...props} />} />
+      </Switch>
+    </Router>
+  )
+};
+
+export default hot(module)(App);
